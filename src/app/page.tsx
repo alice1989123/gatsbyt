f15cc59fe0df5f0fe2777f617bdfad5014b3d9ac
alicia1989123 <aliciabasilo.ab@gmail.com>
2023-04-23 21:09:02 -0600

    updated data api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { createContext } from "react";
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { EChartsOption } from "echarts";
+import { type } from "os";
+import { fetchData } from "../pages/api/crypto"; // Import the function to fetch data
 
 const ReactECharts = dynamic(() => import("echarts-for-react"), { ssr: false });
 
@@ -11,41 +13,38 @@ interface PriceData {
   date: string;
   price: number;
 }
-
+const  api = process.env.API ? process.env.API : "http://localhost:3000/api/crypto";
 const AssetPriceVisualizer = () => {
-  fetch("/api/crypto")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
   const [prices, setPrices] = useState<PriceData[]>([]);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     async function fetchPrices() {
-      //const response = await fetch("/api/prices");
-
-      const data = generateDummyData(); //await response.json();
-      setPrices(data);
+      try {
+        //const res = await fetchData();
+         const response_ = await fetch( api, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }).then((response) => response.json())
+        
+          .then((data) => {setPrices(data.predictions) , setLoading(false) })
+          
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+     
     }
     fetchPrices();
   }, []);
 
-  const generateDummyData = (): PriceData[] => {
-    const data: PriceData[] = [];
-    const startDate = new Date("2022-01-01");
-    const endDate = new Date("2022-03-01");
-    const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+           
+  const minPrice = Math.min(...prices.map(({ price }) => price));
+  const maxPrice = Math.max(...prices.map(({ price }) => price));
 
-    for (let i = 0; i < diffDays; i++) {
-      const currentDate = new Date(
-        startDate.getTime() + i * (1000 * 3600 * 24)
-      );
-      const dateString = currentDate.toISOString().slice(0, 10);
-      const price = Math.floor(Math.random() * 1000) + 1;
-      data.push({ date: dateString, price });
-    }
-
-    return data;
-  };
+  
 
   const options: EChartsOption = {
     title: {
@@ -60,17 +59,42 @@ const AssetPriceVisualizer = () => {
     },
     yAxis: {
       type: "value",
+      min: minPrice,
+      max: maxPrice,
     },
     series: [
       {
-        data: prices.map(({ price }) => price),
+        data: prices.slice(0, -20).map(({ price }) => price),
         type: "line",
         smooth: true,
+        lineStyle: {
+          color: "green",
+        },
+        itemStyle: {
+          color: "green",
+        },
       },
-    ],
+      prices.length > 20 &&{
+        data: Array(prices.length - 20)
+        .fill(null)
+        .concat(prices.slice(-20).map(({ price }) => price)),
+        type: "line",
+        smooth: true,
+        lineStyle: {
+          color: "red",
+        },
+        itemStyle: {
+          color: "red",
+        },
+      },
+    ]
+    
+    
   };
+  
 
   return (
+    loading ? <div>Loading...</div> :
     <div>
       <ReactECharts option={options} />
     </div>
--- src/pages/api/crypto.ts
+++ src/pages/api/crypto.ts
@@ -1,17 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 
+// api/crypto.ts
 
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const ACCESS_TOKEN = 'your-access-token';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await fetchData(); // Call the function to fetch data
+    res.status(200).json(data); // Send the data as a JSON response
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong.' });
+  }
+}
 
-export default async function handler(req: NextApiRequest,
-  res: NextApiResponse) {
-  const response = await fetch("https://api.cryptoquant.com/v1/btc/market-indicator/mvrv?window=day&from=20191001&limit=2" , {
+async function fetchData() {
+  const api = "https://u8x2fk7itj.execute-api.eu-central-1.amazonaws.com/default/crypto_forecast";
+  const response = await fetch(api + "/predict", {
+    method: 'GET',
     headers: {
-      Authorization: `Bearer ${ACCESS_TOKEN}`
+      'Content-Type': 'application/json',
     }
   });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error ${response.status}`);
+  }
+
   const data = await response.json();
-  res.status(200).json(data);
+  return data;
 }
+
+export { fetchData };
