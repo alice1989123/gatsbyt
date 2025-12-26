79bc5b810d30b907c707fdda4f6e1bebb2265ef2
alicia1989123 <aliciabasilo.ab@gmail.com>
2025-12-25 22:00:03 -0600

    test

--- next.config.js
+++ next.config.js
@@ -3,22 +3,16 @@ const nextConfig = {
    experimental: {
     appDir: true,
   },
-  env: {
-    // Make these available in the built server bundle (pages/api)
-    NEXT_CRYPTO_API: process.env.NEXT_CRYPTO_API,
-    NEXT_DEFAULT_REGION: process.env.NEXT_DEFAULT_REGION,
+  // env: {
+  //   // Make these available in the built server bundle (pages/api)
+  //   NEXT_CRYPTO_API: process.env.NEXT_CRYPTO_API,
 
-
-    NEXT_ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
-    NEXT_SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
-
-    // keep your public ones too
-    NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
-    NEXT_PUBLIC_COGNITO_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
-    NEXT_PUBLIC_COGNITO_REDIRECT_URI: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI,
-    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
-    NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-  },
+  //   // keep your public ones too
+  //   NEXT_PUBLIC_COGNITO_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
+  //   NEXT_PUBLIC_COGNITO_REDIRECT_URI: process.env.NEXT_PUBLIC_COGNITO_REDIRECT_URI,
+  //   NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+  //   NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+  // },
 };
 
 module.exports = nextConfig
